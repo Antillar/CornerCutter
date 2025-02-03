d885ac7eea72bdfa56b7b5dbbf96b54685162a88
Antillar <12700019+Antillar@users.noreply.github.com>
2025-02-03 20:37:58 +0300

    Totals, new resolution support and coloration
    * Added a row for totals for each player with actual excel formulas, not manually inserted value from Python
    * Added support for @tel_rivka's ridiculous monitor setup (4480x2160 screenshots)
    * Added coloration for rows with totals and header - greyed background
    * Added coloration for OCR recognition errors - bright red
    * Added common sense checks to draw attention to the possible cases of OCR incorrectly recognising text, set to >=100 for kills/assists and >=10000 for obj (per match). Might need to adjust the latter due to @mjplaysmc

--- CornerCutter.py
+++ CornerCutter.py
@@ -64,6 +64,24 @@ for subdir, dirs, files in os.walk(rootdir):
             O_x2 = 1207
             O_y1 = 1066
             O_y2 = 1104
+            
+        #Tel_Rivka's setup: 2K 16:9 monitor bottom left, on the right 2x FullHD monitors stacked on top of each other. (2560+1920x1)
+        if( img_height > 2160 * 0.9 and img_height < 2160 * 1.1 
+            and img_width > 4480 * 0.9 and img_width < 4480 * 1.1):
+            K_x1 = 1136
+            K_x2 = 1207
+            K_y1 = 1648
+            K_y2 = 1686
+            
+            A_x1 = 1136
+            A_x2 = 1207
+            A_y1 = 1717
+            A_y2 = 1755
+            
+            O_x1 = 1090
+            O_x2 = 1207
+            O_y1 = 1786
+            O_y2 = 1824
         
         #Cuts out all the "corners" except the necessary info
         img_K = rawImg.crop((K_x1, K_y1, K_x2, K_y2))
@@ -111,13 +129,49 @@ for subdir, dirs, files in os.walk(rootdir):
         raw_results += [[player, fileName, int_K, int_A, int_O]]
 
 wb = xlwt.Workbook()
+xlwt.add_palette_colour('errors', 0x21)
+wb.set_colour_RGB(0x21, 255, 0, 0)
+xlwt.add_palette_colour('warnings', 0x22)
+wb.set_colour_RGB(0x22, 255, 255, 0)
+xlwt.add_palette_colour('totals', 0x23)
+wb.set_colour_RGB(0x23, 200, 200, 200)
+style_error = xlwt.easyxf('pattern: pattern solid, fore_colour errors')
+style_warning = xlwt.easyxf('pattern: pattern solid, fore_colour warnings')
+style_total = xlwt.easyxf('pattern: pattern solid, fore_colour totals')
 ws = wb.add_sheet('Raw results')
 header = ['Player', 'Filename', 'Kills', 'Assists', 'Objective score']
 for column, heading in enumerate(header):
-    ws.write(0, column, heading)
+    ws.write(0, column, heading, style_total)
 
+prev_player = ''
+num_blanks = 0
+row_start_player = 0
+row_end_player = 0
 for row, results_array in enumerate(raw_results):
+    if(prev_player != results_array[0]):
+        if(prev_player != ''):
+            row_end_player = row + num_blanks + 1
+            ws.write(row + num_blanks + 1, 0, prev_player, style_total)
+            ws.write(row + num_blanks + 1, 1, 'TOTALS', style_total)
+            ws.write(row + num_blanks + 1, 2, xlwt.Formula('SUM(C' + str(row_start_player) + ':C' + str(row_end_player) +')'), style_total)
+            ws.write(row + num_blanks + 1, 3, xlwt.Formula('SUM(D' + str(row_start_player) + ':D' + str(row_end_player) +')'), style_total)
+            ws.write(row + num_blanks + 1, 4, xlwt.Formula('SUM(E' + str(row_start_player) + ':E' + str(row_end_player) +')'), style_total)
+            num_blanks = num_blanks + 1
+        prev_player = results_array[0]
+        row_start_player = num_blanks + row + 2
     for column, value in enumerate(results_array):
-        ws.write(row + 1, column, value)
+        if(value == -1):
+            ws.write(num_blanks + row + 1, column, value, style_error)
+        else: 
+            if(((column == 2 or column == 3) and value >= 100) or (column == 4 and value >= 10000)):
+                ws.write(num_blanks + row + 1, column, value, style_warning)
+            else:
+                ws.write(num_blanks + row + 1, column, value)
+            
+ws.write(row + num_blanks + 2, 0, prev_player, style_total)
+ws.write(row + num_blanks + 2, 1, 'TOTALS', style_total)
+ws.write(row + num_blanks + 2, 2, xlwt.Formula('SUM(C' + str(row_end_player + 2) + ':C' + str(row + num_blanks + 2) +')'), style_total)
+ws.write(row + num_blanks + 2, 3, xlwt.Formula('SUM(D' + str(row_end_player + 2) + ':D' + str(row + num_blanks + 2) +')'), style_total)
+ws.write(row + num_blanks + 2, 4, xlwt.Formula('SUM(E' + str(row_end_player + 2) + ':E' + str(row + num_blanks + 2) +')'), style_total)
 
 wb.save('RawData.xls')
